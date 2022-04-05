2175542b726cf0fbe9879f15aec786dc09cb7359
Neha <nehavjn@gmail.com>
2022-04-05 19:24:43 +0100

    Update queen.js

--- queen.js
+++ queen.js
@@ -1,137 +1,173 @@
-const buttons = Array.from(document.getElementsByClassName("queen"));
-const checkBtn = document.querySelector("#check-btn");
-const resetBtn = document.querySelector("#reset-btn");
-const selectedButtons = [];
-buttons.forEach(button => {
-    button.id = buttons.indexOf(button) + 1;
-    button.addEventListener('click', () => {
-        const buttonClassList = Array.from(button.classList)
-        const icon = button.firstElementChild;
-        resetBtn.disabled = false;
-        resetBtn.classList.remove('btn-disabled');
-        if (selectedButtons.length === 0 && !buttonClassList.includes('selected')) {
-            icon.classList.remove('inactive');
-            button.classList.add('selected');
-            selectedButtons.push(parseInt(button.id));
-        } else if (selectedButtons.length === 1 && !buttonClassList.includes('selected')) {
-            icon.classList.remove('inactive');
-            button.classList.add('selected');
-            selectedButtons.push(parseInt(button.id));
-            checkBtn.disabled = false;
-            checkBtn.classList.remove('btn-disabled');
-        } else if (selectedButtons.length < 2 || buttonClassList.includes('selected')) {
-            icon.classList.add('inactive');
-            button.classList.remove('selected');
-            selectedButtons.splice( selectedButtons.indexOf(parseInt(button.id)) , 1);
-            checkBtn.disabled = true;
-            checkBtn.classList.add('btn-disabled')
-        }
-    });
-});
+//declare the variables that will be used
+let errorBox = document.querySelector('.error-messages'),
+    successBox = document.querySelector('.success'),
+    failureBox = document.querySelector('.failure'),
+    clickCount = 0;
 
-checkBtn.addEventListener('click', () => {
-    buttons.forEach(button => {
-        button.disabled = true;
-        button.classList.add('disabled')
-    })
-    selectedButtons.sort(function(a, b){return a - b});
-    let position1 = selectedButtons[0];
-    let position2 = selectedButtons[1];
-    let positionDifference = position2 - position1;
-    
-    if ((positionDifference <= 7) && (position1 < 8) && (position2 <= 8)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference <= 7) && (position1 > 8) && (position1 < 16) && (position2 > 8) && (position2 <= 16)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference <= 7) && (position1 > 16) && (position1 < 24) && (position2 > 16) && (position2 <= 24)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference <= 7) && (position1 > 24) && (position1 < 32) && (position2 > 24) && (position2 <= 32)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference <= 7) && (position1 > 32) && (position1 < 40) && (position2 > 32) && (position2 <= 40)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference <= 7) && (position1 > 40) && (position1 < 48) && (position2 > 40) && (position2 <= 48)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference <= 7) && (position1 > 48) && (position1 < 56) && (position2 > 48) && (position2 <= 56)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference <= 7) && (position1 > 56) && (position1 < 64) && (position2 > 56) && (position2 <= 64)) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 1; i < position2; i += 1) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference - 16) % 8 === 0) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 8; i < position2; i += 8) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference - 18) % 9 === 0 && (position1 % 8 != 0) && (((position2 % 8) > (position1 % 8)) || (position2 % 8 === 0))) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 9; i < position2; i += 9) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else if ((positionDifference - 14) % 7 === 0 && (position2 % 8 != 0) && (((position2 % 8) < (position1 % 8)) || (position1 % 8 === 0))) {
-        buttons[position1 - 1].classList.add('under-attack');
-        buttons[position2 - 1].classList.add('under-attack');
-        for (let i = position1 + 7; i < position2; i += 7) {
-            buttons[i - 1].classList.add('attack-path');
-        };
-    } else {
-        buttons[position1 - 1].classList.add('safe');
-        buttons[position2 - 1].classList.add('safe');
+//create the chess board dynamically
+const createBoardLayout = () => {
+
+    let board = document.querySelector('.board-layout'),
+        table = document.createElement('table');
+
+    for (let i = 1; i < 9; i++) {
+
+        let tr = document.createElement('tr');
+
+        for (let j = 1; j < 9; j++) {
+
+            let td = document.createElement('td');
+
+            if (i % 2 === j % 2) {
+                td.className = 'white square';
+                
+                td.id = `square${i}${j}`;
+            } else {
+                td.className = 'black square';
+                td.id = `square${i}${j}`;
+            }
+
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
     }
-});
 
-resetBtn.addEventListener('click', () => {
-    buttons.forEach(button => {
-        button.disabled = false;
-        button.classList.remove('disabled')
-        const buttonClassList = Array.from(button.classList)
-        const icon = button.firstElementChild;
-        if (buttonClassList.includes('selected')) {
-            icon.classList.add('inactive');
-            button.classList.remove('selected');
+    board.appendChild(table);
+};
+
+//The two queens must be in a vertical position to each other for a succesful attack
+const verticalAttack = (q1, q2) => {
+    //returning the selected elements in the column
+    let q1Column =  q1.slice(-1);
+    let q2Column =  q2.slice(-1);
+    return q1Column === q2Column;
+};
+
+//Both queens must be in a diagonal position to each other for a succesful attack
+const diagonalAttack = (q1, q2) => {
+    //returning the selected elements in the row and column
+    let q1Row = q1.slice(0, 1),
+        q1Column = q1.slice(-1);
+
+    let q2Row = q2.slice(0, 1),
+        q2Column = q2.slice(-1);
+
+    return Math.abs(q1Row - q2Row) === Math.abs(q1Column - q2Column);
+};
+
+//The two queens must be in a horizontal position to each other for a succesful attack
+const horizontalAttack = (q1, q2) => {
+    let q1Row = q1.slice(0, 1);
+    let q2Row = q2.slice(0, 1);
+    return q1Row === q2Row;
+};
+
+const canAttack = () => {
+    document.querySelector('.container').style.background = 'palegreen';
+
+    successBox.style.display = 'flex';
+    successBox.innerHTML = `<p>An attack is possible</p>`;
+};
+
+const cannotAttack = () => {
+    document.querySelector('.container').style.background = 'salmon';
+
+    failureBox.style.display = 'flex';
+    failureBox.style.color = 'red';
+    failureBox.innerHTML = `<p>An attack is not possible</p>`;
+};
+
+//checking for attacks and displaying the result
+function checkAttack(firstQueen, secondQueen) {
+    //enable the check button
+    let checkButton = document.querySelector('#checkBtn');
+    checkButton.removeAttribute('disabled');
+    
+    checkButton.addEventListener('click', (e => {
+        
+        e.preventDefault();
+        errorBox.style.display = 'none';
+
+        if ( !verticalAttack(firstQueen, secondQueen) && !horizontalAttack(firstQueen, secondQueen)
+            && !diagonalAttack(firstQueen, secondQueen) ) {
+
+            return cannotAttack();
+
         }
-        if (buttonClassList.includes('under-attack')) {
-            button.classList.remove('under-attack');
+
+        return canAttack();
+
+    }));
+
+}
+
+//listening for a click
+document.addEventListener('click', (e => {
+
+    e.preventDefault();
+
+    let square = e.target.id, // the square that is clicked
+        squareID = square.replace('square', ''); //strip the 'square' and leave the integer for math processing
+    errorBox.textContent = '';
+    ///condition to assign values to each queen
+    if (square.includes('square')) {
+
+        if (clickCount === 0) {
+
+            firstQueen = squareID; //set queen 1 to the value of the first box clicked
+            document.getElementById(square).style.background = 'yellow';
+
+        } else if (clickCount === 1) {
+
+            secondQueen = squareID;
+
+            if (secondQueen === firstQueen) {
+
+                errorBox.style.border = '2px solid red';
+                errorBox.innerHTML = `<p>place the second queen on a different square</p>`;
+                clickCount--;
+
+            } else {
+                errorBox.style.border = '2px solid black';
+                document.getElementById(square).style.background = 'green';
+            }
+
+
+        } else if (clickCount > 1) {
+
+            errorBox.style.border = '2px solid red';
+            errorBox.textContent = 'you cannot have more than two queens';
+
         }
-        if (buttonClassList.includes('attack-path')) {
-            button.classList.remove('attack-path');
+
+        clickCount++;
+
+        if (firstQueen && secondQueen) {
+            return checkAttack(firstQueen, secondQueen);
         }
-        if (buttonClassList.includes('safe')) {
-            button.classList.remove('safe');
+
+    } else if (square.includes('resetBtn')) {
+        clickCount = 0;
+        document.querySelector('#checkBtn').setAttribute('disabled', 'true');
+        window.location = window.location
+
+    } else {
+
+        if (!firstQueen || !secondQueen) {
+
+            errorBox.style.border = '2px solid red';
+            errorBox.innerHTML = `<p>please click inside <br> the chess board</p>`;
         }
-    })
-    selectedButtons.splice(0, selectedButtons.length);
-    checkBtn.disabled = true;
-    checkBtn.classList.add('btn-disabled');
-});
+
+    }
+
+
+}), false);
+
+const startApp = () => {
+    createBoardLayout();
+};
+
+
+startApp();
